2bd198e67150529adb9af8798ae4827ce66abc33
Kai Sawamoto <kaisawamoto@gmail.com>
2020-12-04 23:12:19 +0900

    part1,done

--- main.cpp
+++ main.cpp
@@ -14,9 +14,45 @@
 #define rep(i, n) for (int i = 0; i < n; i++)
 using namespace std;
 
+hitable *random_scene() {
+  int n = 500;
+  vector<hitable *> list;
+  list.push_back(new sphere(point3(0, -1000, 0), 1000,
+                            new lambertian(color(0.5, 0.5, 0.5))));
+  int i = 1;
+  for (int a = -11; a < 11; a++) {
+    for (int b = -11; b < 11; b++) {
+      double choose_mat = drand48();
+      point3 center(a + 0.9 * drand48(), 0.2, b + 0.9 * drand48());
+      if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+        if (choose_mat < 0.8) { // difuse
+          list.push_back(new sphere(
+              center, 0.2,
+              new lambertian(color(drand48() * drand48(), drand48() * drand48(),
+                                   drand48() * drand48()))));
+        } else if (choose_mat < 0.95) { // metal
+          list.push_back(new sphere(
+              center, 0.2,
+              new metal(color(0.5 * (1 + drand48()), 0.5 * (1 + drand48()),
+                              0.5 * (1 + drand48())),
+                        0.5 * (1 + drand48()))));
+        } else {
+          list.push_back(new sphere(center, 0.2, new dielectric(1.5)));
+        }
+      }
+    }
+  }
+  list.push_back(new sphere(point3(0, 1, 0), 1.0, new dielectric(1.5)));
+  list.push_back(
+      new sphere(point3(-4, 1, 0), 1.0, new lambertian(color(0.4, 0.2, 0.1))));
+  list.push_back(
+      new sphere(point3(4, 1, 0), 1.0, new metal(color(0.7, 0.6, 0.5), 0.0)));
+  return new hitable_list(list, list.size());
+}
+
 color getColor(const ray &r, hitable *world, int depth = 0) {
   hit_record rec;
-  if (world->hit(r, 0.01, 1e9, rec)) {
+  if (world->hit(r, 0.001, 1e9, rec)) {
     ray scattered;
     vec3 attenuation;
     if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
@@ -37,19 +73,25 @@ int main() {
   int ny = 100;
   int ns = 100;
   cout << "P3" << endl << nx << " " << ny << endl << 255 << endl;
-  camera cam;
+  vec3 lookfrom(13.0, 2.0, 3.0);
+  vec3 lookat(0.0, 0.0, 0.0);
+  vec3 up_vector(0.0, 1.0, 0.0);
+  double focal_length = (lookfrom - lookat).length();
+  double aparture = 0.1;
+  camera cam(lookfrom, lookat, up_vector, 20, double(nx) / double(ny), aparture,
+             focal_length);
 
-  vector<hitable *> list;
-  list.push_back(new sphere(vec3(0.0, 0.0, -1.0), 0.5,
-                            new lambertian(vec3(0.8, 0.3, 0.3))));
-  list.push_back(new sphere(vec3(0.0, -100.5, -1.0), 100.0,
-                            new lambertian(vec3(0.8, 0.8, 0.0))));
-  list.push_back(
-      new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.3)));
-  list.push_back(new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5)));
-  list.push_back(new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5)));
-  hitable *world = new hitable_list(list, list.size());
-
+  // vector<hitable *> list;
+  // list.push_back(new sphere(vec3(0.0, 0.0, -1.0), 0.5,
+  //                           new lambertian(vec3(0.8, 0.3, 0.3))));
+  // list.push_back(new sphere(vec3(0.0, -100.5, -1.0), 100.0,
+  //                           new lambertian(vec3(0.8, 0.8, 0.0))));
+  // list.push_back(
+  //     new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.3)));
+  // list.push_back(new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5)));
+  // list.push_back(new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5)));
+  // hitable *world = new hitable_list(list, list.size());
+  hitable *world = random_scene();
   rep(i, ny) {
     rep(j, nx) {
       color col;
--- models/camera.h
+++ models/camera.h
@@ -2,24 +2,39 @@
 #define CAMERAH
 
 #include "ray.h"
+#include "vec3.h"
 
 class camera {
 public:
-  camera() {
-    upper_left = point3(-2.0, 1.0, -1.0);
-    origin = point3(0.0, 0.0, 0.0);
-    vert = vec3(0.0, 2.0, 0.0);
-    hori = vec3(4.0, 0.0, 0.0);
+  camera(vec3 &lookfrom, vec3 &lookat, vec3 &up_vector, double fov,
+         double aspect, double aparture, double focal_length) {
+    lens_radius = aparture / 2.0;
+    double theta = fov * M_PI / 180;
+    double half_h = tan(theta / 2.0);
+    double half_w = half_h * aspect;
+    origin = lookfrom;
+    w = unit_vector(lookfrom - lookat);
+    u = unit_vector(cross(up_vector, w));
+    v = cross(w, u);
+    upper_left = origin - focal_length * u * half_w +
+                 focal_length * v * half_h - focal_length * w;
+    hori = 2.0 * focal_length * half_w * u;
+    vert = 2.0 * focal_length * half_h * v;
   }
 
-  ray get_ray(double u, double v) {
-    return ray(origin, upper_left + u * hori - v * vert);
+  ray get_ray(double s, double t) {
+    vec3 rd = lens_radius * random_in_unit_disk();
+    vec3 offset = rd.x() * u + rd.y() * v;
+    return ray(origin + offset,
+               upper_left + s * hori - t * vert - (origin + offset));
   }
 
   point3 upper_left;
   point3 origin;
   vec3 vert;
   vec3 hori;
+  vec3 u, v, w;
+  double lens_radius;
 };
 
 #endif
